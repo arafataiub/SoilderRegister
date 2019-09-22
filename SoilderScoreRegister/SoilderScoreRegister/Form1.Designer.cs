8ab87a4d6368a696e5f6d26b5db12c9f2cf5210e
HOSSAIN-ARMAN <hossainarman4389@gmail.com>
2019-09-22 20:32:26 +0600

    config

---
+++ SoilderScoreRegister/SoilderScoreRegister/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace SoilderScoreRegister
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxSoilderNo = new System.Windows.Forms.TextBox();
+            this.textBoxSoilderName = new System.Windows.Forms.TextBox();
+            this.textBox1Score = new System.Windows.Forms.TextBox();
+            this.textBox2Score = new System.Windows.Forms.TextBox();
+            this.textBox3Score = new System.Windows.Forms.TextBox();
+            this.textBox4Score = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonShowAll = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.radioButtonNo = new System.Windows.Forms.RadioButton();
+            this.radioButtonName = new System.Windows.Forms.RadioButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Soilder No";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soilder Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Target 1 Score";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Target 2 Score";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Target 3 Score";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(50, 269);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Target 4 Score";
+            // 
+            // textBoxSoilderNo
+            // 
+            this.textBoxSoilderNo.Location = new System.Drawing.Point(134, 60);
+            this.textBoxSoilderNo.Name = "textBoxSoilderNo";
+            this.textBoxSoilderNo.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSoilderNo.TabIndex = 6;
+            // 
+            // textBoxSoilderName
+            // 
+            this.textBoxSoilderName.Location = new System.Drawing.Point(134, 96);
+            this.textBoxSoilderName.Name = "textBoxSoilderName";
+            this.textBoxSoilderName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSoilderName.TabIndex = 7;
+            // 
+            // textBox1Score
+            // 
+            this.textBox1Score.Location = new System.Drawing.Point(134, 134);
+            this.textBox1Score.Name = "textBox1Score";
+            this.textBox1Score.Size = new System.Drawing.Size(100, 20);
+            this.textBox1Score.TabIndex = 8;
+            // 
+            // textBox2Score
+            // 
+            this.textBox2Score.Location = new System.Drawing.Point(134, 175);
+            this.textBox2Score.Name = "textBox2Score";
+            this.textBox2Score.Size = new System.Drawing.Size(100, 20);
+            this.textBox2Score.TabIndex = 9;
+            // 
+            // textBox3Score
+            // 
+            this.textBox3Score.Location = new System.Drawing.Point(134, 217);
+            this.textBox3Score.Name = "textBox3Score";
+            this.textBox3Score.Size = new System.Drawing.Size(100, 20);
+            this.textBox3Score.TabIndex = 10;
+            // 
+            // textBox4Score
+            // 
+            this.textBox4Score.Location = new System.Drawing.Point(134, 269);
+            this.textBox4Score.Name = "textBox4Score";
+            this.textBox4Score.Size = new System.Drawing.Size(100, 20);
+            this.textBox4Score.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(469, 28);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Soilder Information";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(469, 63);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(76, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Search Criteria";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(469, 99);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Search By";
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(535, 130);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(142, 20);
+            this.textBoxSearch.TabIndex = 15;
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(207, 325);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 16;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(683, 130);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 17;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // buttonShowAll
+            // 
+            this.buttonShowAll.Location = new System.Drawing.Point(683, 173);
+            this.buttonShowAll.Name = "buttonShowAll";
+            this.buttonShowAll.Size = new System.Drawing.Size(75, 23);
+            this.buttonShowAll.TabIndex = 18;
+            this.buttonShowAll.Text = "Show All";
+            this.buttonShowAll.UseVisualStyleBackColor = true;
+            this.buttonShowAll.Click += new System.EventHandler(this.buttonShowAll_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(493, 396);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(103, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Top Average Scorer";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(493, 419);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(87, 13);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Top Total Scorer";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(602, 393);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 22;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(602, 419);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 23;
+            // 
+            // radioButtonNo
+            // 
+            this.radioButtonNo.AutoSize = true;
+            this.radioButtonNo.Location = new System.Drawing.Point(535, 99);
+            this.radioButtonNo.Name = "radioButtonNo";
+            this.radioButtonNo.Size = new System.Drawing.Size(125, 17);
+            this.radioButtonNo.TabIndex = 24;
+            this.radioButtonNo.TabStop = true;
+            this.radioButtonNo.Text = "radioButtonSoilderNo";
+            this.radioButtonNo.UseVisualStyleBackColor = true;
+            this.radioButtonNo.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButtonName
+            // 
+            this.radioButtonName.AutoSize = true;
+            this.radioButtonName.Location = new System.Drawing.Point(666, 99);
+            this.radioButtonName.Name = "radioButtonName";
+            this.radioButtonName.Size = new System.Drawing.Size(139, 17);
+            this.radioButtonName.TabIndex = 25;
+            this.radioButtonName.TabStop = true;
+            this.radioButtonName.Text = "radioButtonSoilderName";
+            this.radioButtonName.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(459, 214);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(309, 155);
+            this.richTextBox1.TabIndex = 26;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.radioButtonName);
+            this.Controls.Add(this.radioButtonNo);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.buttonShowAll);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox4Score);
+            this.Controls.Add(this.textBox3Score);
+            this.Controls.Add(this.textBox2Score);
+            this.Controls.Add(this.textBox1Score);
+            this.Controls.Add(this.textBoxSoilderName);
+            this.Controls.Add(this.textBoxSoilderNo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxSoilderNo;
+        private System.Windows.Forms.TextBox textBoxSoilderName;
+        private System.Windows.Forms.TextBox textBox1Score;
+        private System.Windows.Forms.TextBox textBox2Score;
+        private System.Windows.Forms.TextBox textBox3Score;
+        private System.Windows.Forms.TextBox textBox4Score;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonShowAll;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.RadioButton radioButtonNo;
+        private System.Windows.Forms.RadioButton radioButtonName;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ SoilderScoreRegister/SoilderScoreRegister/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoilderScoreRegister
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> SoilderNos = new List<string>();
+        List<string> SoilderNames = new List<string>();
+        List<int> T1Scores = new List<int>();
+        List<int> T2Scores = new List<int>();
+        List<int> T3Scores = new List<int>();
+        List<int> T4Scores = new List<int>();
+
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            int error = 0;
+
+            if (textBoxSoilderNo.Text == null)
+            {
+                MessageBox.Show("Please Enter a Unique Soilder No");
+                return;
+            }
+            else if(textBoxSoilderName.Text==null)
+            {
+                MessageBox.Show("Please Enter Soilder Name");
+                return;
+
+            }
+            else if (textBox1Score.Text == null)
+            {
+                MessageBox.Show("Please Enter Target 1 Score");
+                return;
+
+            }
+            else if (textBox2Score.Text == null)
+            {
+                MessageBox.Show("Please Enter Target 2 Score");
+                return;
+
+            }
+            else if (textBox3Score.Text == null)
+            {
+                MessageBox.Show("Please Enter Target 3 Score");
+                return;
+
+            }
+            else if (textBox4Score.Text == null)
+            {
+                MessageBox.Show("Please Enter Target 4 Score");
+                return;
+
+            }
+            else if(CheckDuplicate(textBoxSoilderNo.Text)==true)
+            {
+                MessageBox.Show("Soilder Already Exists!!!");
+                return;
+            }
+            else
+            {
+                try
+                {
+                    Convert.ToInt16(textBox1Score.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Fill Target 1 in correct format");
+                    error = 1;
+                    return;
+                }
+                try
+                {
+                    Convert.ToInt16(textBox2Score.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Fill Target 2 in correct format");
+                    error = 1;
+                    return;
+                }
+                try
+                {
+                    Convert.ToInt16(textBox3Score.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Fill Target 3 in correct format");
+                    error = 1;
+                    return;
+                }
+                try
+                {
+                    Convert.ToInt16(textBox4Score.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Fill Target 4 in correct format");
+                    error = 1;
+                    return;
+                }
+
+                if (error == 0)
+                {
+                    
+                    SoilderNames.Add(textBoxSoilderName.Text);
+                    SoilderNos.Add(textBoxSoilderNo.Text);
+                    T1Scores.Add(Convert.ToInt32(textBox1Score.Text));
+                    T2Scores.Add(Convert.ToInt32(textBox2Score.Text));
+                    T3Scores.Add(Convert.ToInt32(textBox3Score.Text));
+                    T4Scores.Add(Convert.ToInt32(textBox4Score.Text));
+
+                    richTextBox1.Text = "Soilder Info:" + "\n" + "Name:" + textBoxSoilderName.Text + "\n" + "Soilder No:"
+                                       + textBoxSoilderNo.Text + "\n" + "Target 1 Score:" + textBox1Score.Text + "\n"+ "Target 2 Score:" + textBox2Score.Text + "\n"+"Target 3 Score:" + textBox3Score.Text + "\n"+ "Target 4 Score:" + textBox4Score.Text;
+
+                    ClearAllControls();
+                }
+
+                
+
+
+            }
+
+
+        }
+        public bool CheckDuplicate(string cinput)
+        {
+            bool isduplicale = false;
+
+
+            foreach (string c in SoilderNos)
+            {
+                if (c == cinput)
+                {
+                    isduplicale = true;
+                    break;
+                }
+                else
+                    continue;
+            }
+            return isduplicale;
+
+        }
+        private void ClearAllControls()
+        {
+            textBoxSoilderNo.Text = null;
+            textBoxSoilderName.Text = null;
+            textBox1Score.Text = null;
+            textBox2Score.Text = null;
+            textBox3Score.Text = null;
+            textBox4Score.Text = null;
+
+
+        }
+
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonShowAll_Click(object sender, EventArgs e)
+        {
+            string message = "";
+            int topTotall = 0;
+            int topAvg = 0;
+            for (int i = 0; i < SoilderNos.Count(); i++)
+            {
+                message += "Soilder Info:" + "\n" + "Name:" + SoilderNames[i] + "\n" + "Soilder No:"
+                           + SoilderNos[i] + "\n" + "Average Score" +
+                           (T1Scores[i] + T2Scores[i] + T3Scores[i] + T4Scores[i]) / 4 + "\n" + "Totall Score" +
+                           (T1Scores[i] + T2Scores[i] + T3Scores[i] + T4Scores[i]) + "\n";
+            }
+            richTextBox1.Text = message;
+           
+            
+
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            int j = 0;
+            bool found = false;
+            if (radioButtonName.Checked == true || radioButtonNo.Checked == true)
+            {
+                if (radioButtonName.Checked == true)
+                {
+                    foreach (string c in SoilderNames)
+                    {
+                        
+                        if (c == textBoxSearch.Text)
+                        {
+                            string message = "";
+                            
+                                message += "Soilder Info:" + "\n" + "Name:" + SoilderNames[j] + "\n" + "Soilder No:"
+                                           + SoilderNos[j] + "\n" + "Average Score" +
+                                           (T1Scores[j] + T2Scores[j] + T3Scores[j] + T4Scores[j]) / 4 + "\n" + "Totall Score" +
+                                           (T1Scores[j] + T2Scores[j] + T3Scores[j] + T4Scores[j]) + "\n";
+                           
+                            richTextBox1.Text = message;
+                            found = true;
+
+                            break;
+                        }
+                        else
+                        {
+                            j++;
+                            continue;
+                        }
+                            
+                    }
+
+                }
+                if (radioButtonNo.Checked == true)
+                {
+                    foreach (string c in SoilderNos)
+                    {
+
+                        if (c == textBoxSearch.Text)
+                        {
+                            string message = "";
+
+                            message += "Soilder Info:" + "\n" + "Name:" + SoilderNames[j] + "\n" + "Soilder No:"
+                                       + SoilderNos[j] + "\n" + "Average Score" +
+                                       (T1Scores[j] + T2Scores[j] + T3Scores[j] + T4Scores[j]) / 4 + "\n" + "Totall Score" +
+                                       (T1Scores[j] + T2Scores[j] + T3Scores[j] + T4Scores[j]) ;
+
+                            richTextBox1.Text = message;
+                            found = true;
+
+                            break;
+                        }
+                        else
+                        {
+                            j++;
+                            continue;
+                        }
+
+                    }
+
+                }
+
+                if (found == false)
+                {
+                    MessageBox.Show("Soilder Not Found");
+                    return;
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please select a searching criteria!");
+            
+                return;
+            }
+        }
+    }
+}
